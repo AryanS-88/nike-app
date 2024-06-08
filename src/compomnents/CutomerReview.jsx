b56643e9f853fa1439df9e16d46dea7a477a3c20
Aryan <ssingharyan70@gmail.com>
2024-06-08 22:15:54 +0530

    reviews

--- src/compomnents/CutomerReview.jsx
+++ src/compomnents/CutomerReview.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import ReviewCard from '../compomnents/ReviewCard'
+import { reviews } from '../constants'
 
-const CutomerReview = () => {
+const CustomerReviews = () => {
   return (
-    <div>
-      <section className="padding bg-pale-blue">Customer reviews</section>
-    </div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="m-auto mt-4 max-w-lg  text-center info-text">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
-export default CutomerReview
+export default CustomerReviews
